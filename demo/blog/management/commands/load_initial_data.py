ba532ad66245c2e7a72ab44e4e6c4794ffdb1a8c
Thibaud Colas <thibaudcolas@gmail.com>
2026-09-18 13:59:58 +0100

    Fix import sorting in demo site

--- demo/blog/management/commands/load_initial_data.py
+++ demo/blog/management/commands/load_initial_data.py
@@ -9,6 +9,8 @@ from datetime import date
 from pathlib import Path
 from typing import Any
 
+import wagtail
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.core.files.base import ContentFile
@@ -20,8 +22,6 @@ from wagtail.images.models import Image
 from wagtail.models import Collection, Site
 from wagtail.rich_text import RichText
 from wagtail.users.models import UserProfile
-
-import wagtail
 
 from blog.models import BlogIndexPage, BlogPage, Person
 
